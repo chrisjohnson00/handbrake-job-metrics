fa47f30d6e11b46258225483c9bb21a464efe8c5
Chris Johnson <chrisjohnson00@hotmail.com>
2022-09-10 17:08:03 -0700

    feat: collecting metrics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygogo as gogo
+from metric_collector.metric_collector import MetricCollector
 
 # logging setup
 kwargs = {}
@@ -8,6 +9,17 @@ logger = gogo.Gogo('struct', low_formatter=formatter).get_logger(**kwargs)
 
 def main():
     logger.info("Starting")
+    mc = MetricCollector()
+    total_jobs = mc.get_total_jobs()
+    logger.info(f"Job Count {total_jobs}")
+    completed_jobs = mc.get_completed_jobs()
+    logger.info(f"Completed Jobs {completed_jobs}")
+    failed_jobs = mc.get_failed_jobs()
+    logger.info(f"Failed Jobs {failed_jobs}")
+    running_jobs = mc.get_running_jobs()
+    logger.info(f"Running Jobs {running_jobs}")
+    pending_jobs = mc.get_pending_jobs()
+    logger.info(f"Pending Jobs {pending_jobs}")
 
 
 if __name__ == '__main__':
---
+++ metric_collector/metric_collector.py
@@ -0,0 +1,73 @@
+from kubernetes import client, config
+import os
+
+
+class MetricCollector:
+    def __init__(self):
+        if os.environ.get('USE_K8S_CONFIG_FILE'):
+            config.load_kube_config()
+        else:
+            config.load_incluster_config()
+        self.batch_v1_client = client.BatchV1Api()
+        self.core_v1_client = client.CoreV1Api()
+        self.namespace = "handbrake-jobs"
+        self.job_label_selector = "app=handbrake-job"
+        self.job_list = None
+        self.pod_list = None
+
+    def get_total_jobs(self):
+        job_list = self.get_all_jobs()
+        return len(job_list.items)
+
+    def get_all_jobs(self):
+        if not self.job_list:
+            self.job_list = self.batch_v1_client.list_namespaced_job(namespace=self.namespace,
+                                                                     label_selector=self.job_label_selector)
+        return self.job_list
+
+    def get_all_job_pods(self):
+        if not self.pod_list:
+            self.pod_list = self.core_v1_client.list_namespaced_pod(namespace=self.namespace,
+                                                                    label_selector=self.job_label_selector)
+        return self.pod_list
+
+    def get_completed_jobs(self):
+        job_list = self.get_all_jobs()
+        count = 0
+        for job in job_list.items:
+            status = job.status
+            if status.completion_time:
+                count += 1
+        return count
+
+    def get_failed_jobs(self):
+        job_list = self.get_all_jobs()
+        count = 0
+        for job in job_list.items:
+            status = job.status
+            conditions = status.conditions
+            if conditions:
+                for condition in conditions:
+                    if condition.type == "Failed":
+                        count += 1
+        return count
+
+    def get_running_jobs(self):
+        pod_list = self.get_all_job_pods()
+        count = 0
+        for pod in pod_list.items:
+            status = pod.status
+            phase = status.phase
+            if phase == "Running":
+                count += 1
+        return count
+
+    def get_pending_jobs(self):
+        pod_list = self.get_all_job_pods()
+        count = 0
+        for pod in pod_list.items:
+            status = pod.status
+            phase = status.phase
+            if phase == "Pending":
+                count += 1
+        return count
